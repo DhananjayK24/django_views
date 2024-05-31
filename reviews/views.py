6cdc65add87776e73a6875927fc0dd97a434b7d2
Dhananjay Katare <dhananjaykatare24@gmail.com>
2024-05-31 15:29:54 +0530

    Template Views

---
+++ reviews/views.py
@@ -0,0 +1,47 @@
+from typing import Any
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.views import View
+from django.views.generic.base import TemplateView
+
+from .forms import ReviewForm
+from .models import Review
+
+# Create your views here.
+
+
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
+
+    def post(self, request):
+        form = ReviewForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/thank-you")
+        
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
+
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = "It Worked!"
+        return context
+
+class ReviewListView(TemplateView):
+    template_name = "reviews/reviews_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        reviews = Review.objects.all()
+        context["reviews"] = reviews
+        return context
